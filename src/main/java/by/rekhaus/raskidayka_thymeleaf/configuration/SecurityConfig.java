dbbb1bd7c6547bb7f35c017ca093f47b200172dc
Korney-EV <korneychik.e.v@gmail.com>
2021-12-30 22:48:29 +0300

    Корректировка 1

--- src/main/java/by/rekhaus/raskidayka_thymeleaf/configuration/SecurityConfig.java
+++ src/main/java/by/rekhaus/raskidayka_thymeleaf/configuration/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/js/**", "/css/**", "/img/**").permitAll()
+                .antMatchers("/", "/home", "/js/**", "/css/**", "/img/**", "/blog", "/contact", "/about").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
